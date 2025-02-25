8763636f99486c1c70803287c86702e25498cf78
Atadewa <aditafli42@gmail.com>
2025-02-25 16:23:37 +0700

    feat: update UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,10 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index(){
+        // Add user data with Eloquent Model
+        $data = [
+            'username' => 'customer-1',
+            'nama' => 'Pelanggan',
+            'password' => Hash::make('12345'),
+            'level_id' => 4
+        ];
+        UserModel::insert($data); // Add data to m_user table
+
         $user = UserModel::all(); // Get all data from table m_user
         return view('user', ['data' => $user]);
     }
