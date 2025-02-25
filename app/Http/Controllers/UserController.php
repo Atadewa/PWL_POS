1a96e7a39f6dfe25d4c16c4ebc26da8fbc68ad6d
Atadewa <aditafli42@gmail.com>
2025-02-25 16:55:14 +0700

    feat: update UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,13 +10,18 @@ class UserController extends Controller
 {
     public function index(){
         // Add user data with Eloquent Model
+        // $data = [
+        //     'username' => 'customer-1',
+        //     'nama' => 'Pelanggan',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 4
+        // ];
+        // UserModel::insert($data); // Add data to m_user table
+
         $data = [
-            'username' => 'customer-1',
-            'nama' => 'Pelanggan',
-            'password' => Hash::make('12345'),
-            'level_id' => 4
+            'nama' => 'Pelanggan Pertama',
         ];
-        UserModel::insert($data); // Add data to m_user table
+        UserModel::where('username', 'customer-1')->update($data); // Update data user
 
         $user = UserModel::all(); // Get all data from table m_user
         return view('user', ['data' => $user]);
