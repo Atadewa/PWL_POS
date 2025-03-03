6d797106b53bc2969d8ab5e06147147586f7aeeb
Atadewa <aditafli42@gmail.com>
2025-03-03 21:38:13 +0700

    feat: update UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,15 +9,7 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
-
-        $user = UserModel::all(); // Get all data from table m_user
+        $user = UserModel::find(1);
         return view('user', ['data' => $user]);
     }
 }
