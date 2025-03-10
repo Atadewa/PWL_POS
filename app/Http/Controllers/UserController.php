062f4b83744b2ffed77bb14cbceb0e0eefc814c2
Atadewa <aditafli42@gmail.com>
2025-03-10 21:17:39 +0700

    feat: add store method in UserController to handle user creation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -62,4 +62,23 @@ class UserController extends Controller
 
         return view('user.create', ['breadcrumb' => $breadcrumb, 'page' => $page, 'level' => $level,'activeMenu' => $activeMenu]);
     }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'username' => 'required|string|min:3|unique:m_user,username',
+            'nama'     => 'required|string|max:100',
+            'password' => 'required|min:5', 
+            'level_id' => 'required|integer' 
+        ]);
+
+        UserModel::create([
+            'username' => $request->username,
+            'nama'     => $request->nama,
+            'password' => bcrypt($request->password),
+            'level_id' => $request->level_id
+        ]);
+
+        return redirect('/user')->with('success', 'Data user berhasil disimpan');
+    }
 }
