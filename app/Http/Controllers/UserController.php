e86348a7c3be213767fbefc59b610ee7e1a32df7
Atadewa <aditafli42@gmail.com>
2025-03-17 20:04:48 +0700

    feat: add delete_ajax method for asynchronous user deletion

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -250,4 +250,24 @@ class UserController extends Controller
 
         return view('user.confirm_ajax', ['user' => $user]);
     }
+
+    public function delete_ajax(Request $request, $id)
+    {
+        if ($request->ajax() || $request->wantsJson()) {
+            $user = UserModel::find($id);
+            if ($user) {
+                $user->delete();
+                return response()->json([
+                    'status' => true,
+                    'message' => 'Data berhasil dihapus'
+                ]);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Data tidak ditemukan'
+                ]);
+            }
+        }
+        return redirect('/');
+    }
 }
