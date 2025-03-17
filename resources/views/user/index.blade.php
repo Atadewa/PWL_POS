120b701cf0baba918c4325fd6a06ea618bcf4ebc
Atadewa <aditafli42@gmail.com>
2025-03-17 16:22:16 +0700

    feat: add modal action for user creation via AJAX

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -60,8 +60,9 @@
       }); 
     }
 
+    var dataUser;
     $(document).ready(function() { 
-      var dataUser = $('#table_user').DataTable({ 
+      dataUser = $('#table_user').DataTable({ 
           // serverSide: true, jika ingin menggunakan server side processing 
           serverSide: true,      
           ajax: { 
