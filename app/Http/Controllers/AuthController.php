065a9f679317a195ce71368c5e8f2e4edc6e8545
Atadewa <aditafli42@gmail.com>
2025-03-26 22:52:08 +0700

    feat(auth): implement user registration functionality

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,13 @@
  
 namespace App\Http\Controllers; 
  
+use App\Models\LevelModel;
+use App\Models\UserModel;
 use Illuminate\Http\Request; 
-use Illuminate\Support\Facades\Auth; 
- 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
 class AuthController extends Controller 
 { 
     public function login() 
@@ -45,4 +49,46 @@ class AuthController extends Controller
         $request->session()->regenerateToken();     
         return redirect('login'); 
     } 
+
+    public function register() 
+    {
+        if(Auth::check()){
+            return redirect('/'); 
+        }
+
+        $level = LevelModel::select('level_id', 'level_nama')->get();
+
+        return view('auth.register')->with('level', $level);
+    }
+
+    public function postRegister(Request $request) 
+    {
+        if($request->ajax() || $request->wantsJson()){
+            $rules = [
+                'level_id' => 'required|integer',
+                'username' => 'required|string|min:3|unique:m_user,username',
+                'nama'     => 'required|string|max:100',
+                'password' => 'required|min:5'
+            ];
+    
+            $validator = Validator::make($request->all(), $rules);
+    
+            if($validator->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Validasi Gagal',
+                    'msgField' => $validator->errors(),
+                ]);
+            }
+    
+            $user = UserModel::create($request->all());
+            Auth::login($user);
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Register Berhasil'
+            ]);
+        }
+        redirect('/');
+    }
 } 
