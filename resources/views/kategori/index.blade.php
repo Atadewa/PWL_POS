013654c709ce2e3f27e7719d3d9a6e6f990d6438
Atadewa <aditafli42@gmail.com>
2025-04-10 14:37:55 +0700

    refactor: enhance kategori.index layout and add import feature

--- resources/views/kategori/index.blade.php
+++ resources/views/kategori/index.blade.php
@@ -1,85 +1,94 @@
 @extends('layouts.template') 
  
 @section('content') 
-  <div class="card card-outline card-primary"> 
-      <div class="card-header"> 
-        <h3 class="card-title">{{ $page->title }}</h3> 
-        <div class="card-tools"> 
-          <a class="btn btn-sm btn-primary mt-1" href="{{ url('kategori/create') }}">Tambah</a>
-          <button onclick="modalAction('{{ url('kategori/create_ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah Ajax</button>
+    <div class="card"> 
+        <div class="card-header"> 
+            <h3 class="card-title">{{ $page->title }}</h3> 
+            <div class="card-tools"> 
+                <button onclick="modalAction('{{ url('/kategori/import') }}')" class="btn btn-info">Import Kategori</button> 
+                <a href="{{ url('/kategori/create') }}" class="btn btn-primary">Tambah Data</a> 
+                <button onclick="modalAction('{{ url('/kategori/create_ajax') }}')" class="btn btn-success">Tambah Data (Ajax)</button> 
+            </div> 
         </div> 
-      </div> 
-      <div class="card-body"> 
-        @if (session('success'))
-          <div class="alert alert-success">{{ session('success') }}</div>
-        @endif 
-        @if (session('error'))
-          <div class="alert alert-danger">{{ session('error') }}</div>
-        @endif
-        <table class="table table-bordered table-striped table-hover table-sm" id="table_kategori"> 
-          <thead> 
-            <tr>
-              <th>ID</th>
-              <th>Kode kategori</th>
-              <th>Nama Kategori</th>
-              <th>Aksi</th>
-            </tr> 
-          </thead> 
-        </table> 
-      </div> 
-  </div> 
-
-  <div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data-backdrop="static" data-keyboard="false" data-width="75%" aria-hidden="true"></div> 
+        <div class="card-body">  
+          @if(session('success')) 
+              <div class="alert alert-success">{{ session('success') }}</div> 
+          @endif 
+          @if(session('error')) 
+              <div class="alert alert-danger">{{ session('error') }}</div> 
+          @endif
+          <table class="table table-bordered table-sm table-striped table-hover" id="table-kategori"> 
+            <thead> 
+              <tr>
+                <th>ID</th>
+                <th>Kode Kategori</th>
+                <th>Nama Kategori</th>
+                <th>Aksi</th>
+              </tr> 
+            </thead> 
+            <tbody></tbody> 
+          </table> 
+        </div> 
+    </div> 
+    <div id="myModal" class="modal fade animate shake" tabindex="-1" data-backdrop="static" 
+data-keyboard="false" data-width="75%"></div> 
+     
 @endsection 
  
-@push('css') 
-@endpush 
- 
 @push('js') 
-  <script>
+<script> 
     function modalAction(url = ''){ 
-      $('#myModal').load(url,function(){ 
-          $('#myModal').modal('show'); 
-      }); 
+        $('#myModal').load(url,function(){ 
+            $('#myModal').modal('show'); 
+        }); 
     } 
-
-    var dataKategori;
-    $(document).ready(function() { 
-      dataKategori = $('#table_kategori').DataTable({ 
-        // serverSide: true, jika ingin menggunakan server side processing 
-        serverSide: true,      
+ 
+var tableKategori; 
+$(document).ready(function(){ 
+    tableKategori = $('#table-kategori').DataTable({ 
+        processing: true, 
+        serverSide: true, 
         ajax: { 
             "url": "{{ url('kategori/list') }}", 
             "dataType": "json", 
-            "type": "POST" 
+            "type": "POST", 
+            "data": function (d) { 
+                d.filter_kategori = $('.filter_kategori').val(); 
+            } 
         }, 
-        columns: [ 
-          {
-            // nomor urut dari laravel datatable addIndexColumn() 
-            data: "DT_RowIndex",             
-            className: "text-center", 
-            orderable: false, 
-            searchable: false     
-          }, { 
-            data: "kategori_kode",                
-            className: "", 
-            // orderable: true, jika ingin kolom ini bisa diurutkan  
-            orderable: true,     
-            // searchable: true, jika ingin kolom ini bisa dicari 
-            searchable: true     
-          }, { 
-            data: "kategori_nama",                
-            className: "", 
-            orderable: true,     
-            searchable: true         
-          }, { 
-            data: "aksi",                
-            className: "", 
-            orderable: false,     
-            searchable: false     
-          } 
+        columns: [{ 
+                data: "kategori_id",  
+                className: "text-center", 
+                width: "5%", 
+                orderable: false, 
+                searchable: false 
+            },{ 
+                data: "kategori_kode",  
+                className: "", 
+                width: "20%",
+                orderable: true, 
+                searchable: true 
+            },{ 
+                data: "kategori_nama",  
+                className: "", 
+                width: "55%",
+                orderable: true, 
+                searchable: true, 
+            },{ 
+                data: "aksi",  
+                className: "text-center", 
+                width: "20%", 
+                orderable: false, 
+                searchable: false 
+            } 
         ] 
-      });
     }); 
-  </script> 
-@endpush 
+ 
+    $('#table-kategori_filter input').unbind().bind().on('keyup', function(e){ 
+        if(e.keyCode == 13){ // enter key 
+            tableKategori.search(this.value).draw(); 
+        } 
+    }); 
+}); 
+</script> 
+@endpush
