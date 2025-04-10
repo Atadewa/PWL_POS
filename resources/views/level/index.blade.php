8b04e231fd5c171f55b069df1e26f0f979e6be6e
Atadewa <aditafli42@gmail.com>
2025-04-10 18:08:32 +0700

    feat: update "Tambah Data" button to "Export Level"

--- resources/views/level/index.blade.php
+++ resources/views/level/index.blade.php
@@ -6,7 +6,7 @@
             <h3 class="card-title">{{ $page->title }}</h3> 
             <div class="card-tools"> 
                 <button onclick="modalAction('{{ url('/level/import') }}')" class="btn btn-info">Import Level</button> 
-                <a href="{{ url('/level/create') }}" class="btn btn-primary">Tambah Data</a> 
+                <a href="{{ url('/level/export_excel') }}" class="btn btn-primary"><i class="fa fa-file-excel"> Export Level</i></a>  
                 <button onclick="modalAction('{{ url('/level/create_ajax') }}')" class="btn btn-success">Tambah Data (Ajax)</button> 
             </div> 
         </div> 
@@ -20,7 +20,7 @@
           <table class="table table-bordered table-sm table-striped table-hover" id="table-level"> 
             <thead> 
               <tr>
-                <th>ID</th>
+                <th>No</th>
                 <th>Kode Level</th>
                 <th>Level</th>
                 <th>Aksi</th>
@@ -43,9 +43,9 @@ data-keyboard="false" data-width="75%"></div>
         }); 
     } 
  
-var tableLevel; 
+var dataLevel; 
 $(document).ready(function(){ 
-    tableLevel = $('#table-level').DataTable({ 
+    dataLevel = $('#table-level').DataTable({ 
         processing: true, 
         serverSide: true, 
         ajax: { 
@@ -57,7 +57,7 @@ $(document).ready(function(){
             } 
         }, 
         columns: [{ 
-                data: "level_id",  
+                data: "DT_RowIndex",  
                 className: "text-center", 
                 width: "5%", 
                 orderable: false, 
@@ -85,7 +85,7 @@ $(document).ready(function(){
  
     $('#table-level_filter input').unbind().bind().on('keyup', function(e){ 
         if(e.keyCode == 13){ // enter key 
-            tableLevel.search(this.value).draw(); 
+            dataLevel.search(this.value).draw(); 
         } 
     }); 
 }); 
